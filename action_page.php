2239d52a98a058b3eac0f91aed6cb0f392cd641c
Ala <maatouk.ala@gmail.com>
2021-03-26 16:26:23 +0100

    chiheb1

---
+++ action_page.php
@@ -0,0 +1,27 @@
+
+
+
+ 
+
+
+ 
+
+<?php
+
+
+
+  $aDoor = $_POST['lname'];
+//   if(empty($aDoor)) 
+//   {
+//     echo("You didn't select any buildings.");
+//   } 
+   $i= explode(',',$aDoor[0])     ;
+   $N = count($i);
+
+    // echo("You selected $N door(s): ");
+    // for($z=0; $z < $N; $z++)
+    // {
+    //   echo($i[$z] . " ");
+    // }
+  
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,42 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'analog/analog' => 
+    array (
+      'pretty_version' => '1.0.17-stable',
+      'version' => '1.0.17.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f9541cdf30dfb391a8be1befb05a3344099e6ea9',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.3',
+      'version' => '1.1.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f73288fd15629204f9d42b7055f72dacbe811fc',
+    ),
+  ),
+);
