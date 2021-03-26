ffb4baa0c64b82b81c0200516ad7ce487257f8cc
AhmedHammami7 <ahmed.hammami@etudiant-fst.utm.tn>
2021-03-26 21:26:08 +0100

    hh

--- index.php
+++ index.php
@@ -29,14 +29,20 @@
     <div class="carousel-item active dark-overlay">
       <img class="d-block w-100" src="img/movie-thumb-7.jpg" alt="First slide">
       <div class="carousel-content">
-        <p><h1>The <br> Irishman </h1> </p>
+        <p>The <br> Irishman </p>
       </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="img/movie-thumb-5.jpg" alt="Second slide">
+      <div class="carousel-content">
+        <p>VICE</p>
+      </div>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="img/movie-thumb-3.jpg" alt="Third slide">
+      <div class="carousel-content">
+        <p>The<br>Lego Movie</p>
+      </div>
     </div>
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
