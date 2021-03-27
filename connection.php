570984efe27893bd69f1a3ea68039bd9f52141ff
Chiheb Rinez <shyhebrinez@gmail.com>
2021-03-27 04:25:38 +0100

    footer

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 $host = "localhost"; /* Host name */
 $user = "root"; /* User */
 $password = ""; /* Password */
-$dbname = "movie_git"; /* Database name */
+$dbname = "cinema_db"; /* Database name */
 
 $con = mysqli_connect($host, $user, $password,$dbname);
 // Check connection
